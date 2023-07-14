64fd29a35a6e3cccd2e151c0dbb08691d3bf0e59
Evnomad <b.boy.bip@gmail.com>
2023-07-14 23:56:39 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+'strict';
+const $ = require('jquery');
+window.jQuery = $;
+window.$ = $;
+
+import './scss/main.scss';
+import './js/assets';
+
+(function () {
+  window.addEventListener('DOMContentLoaded', () => {
+    
+  });
+
+})();
